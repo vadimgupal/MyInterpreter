7b1ddf1d692559fb5fe54269bd19cee2c9c649dd
vadimgupal <114927468+vadimgupal@users.noreply.github.com>
2025-05-11 21:03:46 +0300

    fix: added ElementTyp for the type of elements in the array

--- src/compile/Types/SymbolInfo.java
+++ src/compile/Types/SymbolInfo.java
@@ -6,18 +6,21 @@ public class SymbolInfo {
     public String Name;
     public KindType Kind;
     public SemanticType Typ;
+    public SemanticType ElementTyp;
     public List<SemanticType> Params;
 
     public SymbolInfo(String name, KindType kind, SemanticType type) {
         this.Name = name;
         this.Kind = kind;
         this.Typ = type;
+        ElementTyp = null;
         this.Params = null;
     }
 
     public SymbolInfo(String name, KindType kind, List<SemanticType> params, SemanticType type) {
         this.Name = name;
         this.Kind = kind;
+        ElementTyp = null;
         this.Params = params;
         this.Typ = type;
     }
